e076ae3eac0f6b3f6d660d6d4d7d23d899b68822
Tanmay choudhari <89440891+tannmayyy@users.noreply.github.com>
2025-02-17 08:44:10 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,13 @@
-st.markdown('<span style="color: orange;">&#9888; No data to preview with this query!</span>', unsafe_allow_html=True)
+st.markdown(
+    """
+    <div style="
+        background-color: #FFF3CD;
+        color: #856404;
+        padding: 10px;
+        border-radius: 5px;
+        border-left: 5px solid #FFDD57;">
+        &#9888; No data to preview with this query!
+    </div>
+    """,
+    unsafe_allow_html=True
+)
