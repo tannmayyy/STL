b7441b7c7793ee1936711d3ebc610029139499b8
Tanmay choudhari <89440891+tannmayyy@users.noreply.github.com>
2025-02-17 20:32:20 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,6 @@
 with ThreadPoolExecutor(max_workers=2) as executor:
-    future_count = executor.submit(lambda: execute_query(st.session_state.pre_result), conn)
-    future_data = executor.submit(lambda: execute_query(st.session_state.sql_query), conn)
+    future_count = executor.submit(lambda conn=conn: execute_query(st.session_state.pre_result), conn)
+    future_data = executor.submit(lambda conn=conn: execute_query(st.session_state.sql_query), conn)
 
     result = future_count.result()
     data = future_data.result()
